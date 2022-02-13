56acb414c848cdf9feee281e7d7421a841dc3e24
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-02-13 18:11:01 -0300

    Solução do problema Carros Iguais;

--- Carros Iguais/Program.cs
+++ Carros Iguais/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main()
+    {
+        var solucao = SolutionSegundo(new string[] { "100", "110", "010", "011", "100" });
+        foreach (var item in solucao)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    internal static int[] SolutionSegundo(string[] cars)
+    {
+        int[] arrInt = new int[cars.Length];
+        for (int i = 0; i < cars.Length; i++)
+        {
+            for (int j = i; j < cars.Length; j++)
+            {
+                if (i != j && IsEquals(cars[i], cars[j]))
+                {
+                    arrInt[i]++;
+                    arrInt[j]++;
+                }
+            }
+        }
+        return arrInt;
+    }
+
+    internal static bool IsEquals(string a, string b)
+    {
+        int diff = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+                diff++;
+            if (diff > 1)
+                return false;
+        }
+        return true;
+    }
+}
